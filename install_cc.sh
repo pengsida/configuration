f5b9764356ba5fe4185b5627698b333aec52fa34
pengsida <pengsida@zju.edu.cn>
2019-07-15 14:14:58 +0800

    update

--- install_cc.sh
+++ install_cc.sh
@@ -4,6 +4,7 @@ git clone --recursive https://github.com/cloudcompare/trunk.git
 sudo apt-get install -y build-essential
 sudo apt-get install -y qt5-default
 sudo apt-get install -y libqt5svg5-dev
+sudo apt-get install -y qttools5-dev-tools
 sudo apt-get install -y cmake
 
 cd trunk
